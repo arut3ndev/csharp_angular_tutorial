6434de60926393a93bbad9bbd6834b9d2bc5f64e
arut3n <arut3ncodes@gmail.com>
2026-08-26 16:55:31 +0200

    Finished Stage 1 of the tutorial project

--- TestApi/Controllers/TaskItemController.cs
+++ TestApi/Controllers/TaskItemController.cs
@@ -7,12 +7,25 @@ namespace TestApi
     public class TaskItemController : ControllerBase
     {
         [HttpGet]
+        [Route("GetTasks")]
         public IActionResult GetTasks() 
         {
             List<TaskItem> taskItems = new List<TaskItem>();
             TaskItem taskItem = new TaskItem(
-                
-            )
+                1,
+                "Sample Task",
+                false,
+                DateTime.Now
+            );
+            TaskItem taskItem2 = new TaskItem(
+                2,
+                "Sample Task 2",
+                true,
+                DateTime.Today.AddDays(-1)
+            );
+            taskItems.Add(taskItem);
+            taskItems.Add(taskItem2);
+            return Ok(taskItems);
         }
     }
 }
--- TestApi/Models/TaskItem.cs
+++ TestApi/Models/TaskItem.cs
@@ -1,11 +1,23 @@
-class TaskItem 
+public class TaskItem
 {
-    TaskItem()
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public TaskItem()
     {
-        
+        Id = 0;
+        Title = string.Empty;
+        IsCompleted = false;
+        CreatedAt = DateTime.Now;
     }
-    public int Id {get; set;}
-    public string Title {get; set;}
-    public bool IsCompleted {get; set;}
-    public DateTime CreatedAt {get; set;}
+
+    public TaskItem(int id, string title, bool isCompleted, DateTime createdAt)
+    {
+        Id = id;
+        Title = title;
+        IsCompleted = isCompleted;
+        CreatedAt = createdAt;
+    }
 }
